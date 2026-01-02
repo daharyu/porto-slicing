566680a11425727e8f04071b00e73c8df810fae4
daviddev <dharison05@gmail.com>
2026-01-02 22:35:25 +0700

    update footer logo

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,6 +1,7 @@
 import { Dribbble, Instagram, Linkedin } from 'lucide-react';
 import Image from 'next/image';
-import React from 'react';
+import meImg from '@/../public/images/Me1.png';
+import Link from 'next/link';
 
 const FooterPage = () => {
   return (
@@ -8,12 +9,20 @@ const FooterPage = () => {
       <footer className='bg-primary-400 px-xl py-5xl text-neutral-25 gap-2xl md:px-10xl md:py-3xl flex w-full flex-col justify-between md:flex-row md:items-center'>
         <div className='gap-2xl md:gap-xl flex flex-col md:flex-row'>
           {/* Logo */}
-          <div className='flex gap-[8.53px]'>
-            <Image src='/logo-symbol.svg' alt='logo' width={27} height={29} />
-            <h5 className='leading-4xl text-[21.33px] font-semibold'>
-              Your Logo
-            </h5>
-          </div>
+          <Link href='/' className='flex gap-[8.53px]'>
+            <Image
+              src={meImg}
+              alt='logo'
+              width={100}
+              height={100}
+              className={`size-8 rounded-full border border-white bg-white object-center`}
+            />
+            <div
+              className={`leading-4xl text-[21.33px] font-semibold text-white`}
+            >
+              David H Y
+            </div>
+          </Link>
 
           {/* Copyright */}
           <p className='text-sm leading-7'>
